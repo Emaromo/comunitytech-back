f39dea29fa2104fe4a726e24266c5276ad61accf
Ema Romo <ema_romo97@hotmail.com>
2025-11-10 13:05:27 -0300

    Limpieza del repo - sólo backend Java

--- frontend/src/utils/utils.js
+++
@@ -1,7 +0,0 @@
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge"; // ✅ viene de esta librería
-
-// cn = combina clases de forma inteligente (Tailwind + condicionales)
-export function cn(...inputs) {
-  return twMerge(clsx(inputs));
-}
